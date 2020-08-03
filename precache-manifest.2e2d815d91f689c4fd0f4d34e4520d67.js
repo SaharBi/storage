5fb7fb56f14213645f849bdd8eca1cf1599dd6a5
Sahar Bitton <saharbi@walla.co.il>
2020-08-03 12:12:01 +0300

    Updates

---
+++ precache-manifest.2e2d815d91f689c4fd0f4d34e4520d67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e7613f71bb013656e7029abee771561",
+    "url": "/storage/index.html"
+  },
+  {
+    "revision": "1e9d16b446e3778b261c",
+    "url": "/storage/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b488d1b393e72ab86826",
+    "url": "/storage/static/css/main.1daac289.chunk.css"
+  },
+  {
+    "revision": "1e9d16b446e3778b261c",
+    "url": "/storage/static/js/2.3cfecc56.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/storage/static/js/2.3cfecc56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b488d1b393e72ab86826",
+    "url": "/storage/static/js/main.2e3be813.chunk.js"
+  },
+  {
+    "revision": "1fd0bbbbe71bb53887c2",
+    "url": "/storage/static/js/runtime-main.90b7f5c4.js"
+  }
+]);
